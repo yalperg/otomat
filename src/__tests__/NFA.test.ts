2b439b493e192ac5fb022bded3180a7b074f5e06
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2024-08-29 23:14:16 +0300

    test(NFA): add new test for other NFA methods & change toDFA tests

--- src/__tests__/NFA.test.ts
+++ src/__tests__/NFA.test.ts
@@ -3,105 +3,171 @@ import State from "../State";
 import Transition from "../Transition";
 import Alphabet from "../Alphabet";
 
-describe("NFA toDFA method", () => {
-  test("should convert an NFA with a single state and no transitions to a DFA", () => {
-    const stateA = new State("A");
-    const nfa = new NFA([stateA], [], stateA, [stateA], new Alphabet([]));
+describe("NFA class", () => {
+  describe("toDFA method", () => {
+    test("should convert an NFA with a single state and no transitions to a DFA", () => {
+      const stateA = new State("A");
+      const nfa = new NFA([stateA], [], stateA, [stateA], new Alphabet([]));
 
-    const dfa = nfa.toDFA();
+      const dfa = nfa.toDFA(true);
 
-    expect(dfa.states).toHaveLength(1);
-    expect(dfa.states[0].name).toBe("A");
-    expect(dfa.startState).toBe(dfa.states[0]);
-    expect(dfa.acceptStates).toEqual(dfa.states);
-    expect(dfa.transitions).toHaveLength(0);
+      expect(dfa.states).toHaveLength(1);
+      expect(dfa.states[0].name).toBe("A");
+      expect(dfa.startState).toBe(dfa.states[0]);
+      expect(dfa.acceptStates).toEqual(dfa.states);
+      expect(dfa.transitions).toHaveLength(0);
+    });
+
+    test("should convert an NFA with epsilon transitions to a DFA", () => {
+      const stateA = new State("A");
+      const stateB = new State("B");
+      const stateC = new State("C");
+
+      const transitions = [
+        new Transition(stateA, stateB, "ε"),
+        new Transition(stateB, stateC, "ε"),
+      ];
+
+      const alphabet = new Alphabet(["ε"]);
+      const nfa = new NFA(
+        [stateA, stateB, stateC],
+        transitions,
+        stateA,
+        [stateC],
+        alphabet,
+      );
+
+      const dfa = nfa.toDFA(true);
+
+      expect(dfa.states).toHaveLength(1);
+      expect(dfa.states[0].name).toBe("A,B,C");
+      expect(dfa.startState!.name).toBe("A,B,C");
+      expect(dfa.acceptStates).toEqual(dfa.states);
+      expect(dfa.transitions).toHaveLength(0);
+    });
+
+    test("should convert an NFA with multiple states and transitions to a DFA", () => {
+      const stateA = new State("A");
+      const stateB = new State("B");
+      const stateC = new State("C");
+
+      const transitions = [
+        new Transition(stateA, stateB, "0"),
+        new Transition(stateB, stateC, "1"),
+        new Transition(stateC, stateA, "0"),
+        new Transition(stateA, stateC, "1"),
+        new Transition(stateB, stateA, "0"),
+      ];
+
+      const alphabet = new Alphabet(["0", "1"]);
+      const nfa = new NFA(
+        [stateA, stateB, stateC],
+        transitions,
+        stateA,
+        [stateC],
+        alphabet,
+      );
+
+      const dfa = nfa.toDFA(true);
+      expect(dfa.states.length).toBeGreaterThan(0);
+      expect(dfa.startState!.name).toBe("A");
+      expect(
+        dfa.acceptStates.some((state) => state.name.includes("C")),
+      ).toBeTruthy();
+      expect(dfa.transitions.length).toBeGreaterThan(0);
+    });
+
+    test("should remove unreachable states in DFA", () => {
+      const stateA = new State("A");
+      const stateB = new State("B");
+      const stateC = new State("C");
+      const stateD = new State("D");
+
+      const transitions = [
+        new Transition(stateA, stateB, "0"),
+        new Transition(stateB, stateC, "1"),
+        new Transition(stateC, stateA, "0"),
+        new Transition(stateA, stateC, "1"),
+        new Transition(stateB, stateA, "0"),
+        new Transition(stateC, stateB, "1"),
+        new Transition(stateD, stateD, "0"), // Unreachable state
+        new Transition(stateD, stateD, "1"), // Unreachable state
+      ];
+
+      const alphabet = new Alphabet(["0", "1"]);
+      const nfa = new NFA(
+        [stateA, stateB, stateC, stateD],
+        transitions,
+        stateA,
+        [stateC],
+        alphabet,
+      );
+
+      const dfa = nfa.toDFA(true);
+
+      expect(dfa.states).not.toContainEqual(
+        expect.objectContaining({ name: "D" }),
+      );
+      expect(dfa.transitions).not.toContainEqual(
+        expect.objectContaining({
+          from: expect.objectContaining({ name: "D" }),
+        }),
+      );
+    });
   });
 
-  test("should convert an NFA with epsilon transitions to a DFA", () => {
-    const stateA = new State("A");
-    const stateB = new State("B");
-    const stateC = new State("C");
+  describe("epsilonClosure method", () => {
+    test("should compute epsilon closure correctly", () => {
+      const stateA = new State("A");
+      const stateB = new State("B");
+      const stateC = new State("C");
 
-    const transitions = [
-      new Transition(stateA, stateB, "ε"),
-      new Transition(stateB, stateC, "ε"),
-    ];
+      const transitions = [
+        new Transition(stateA, stateB, "ε"),
+        new Transition(stateB, stateC, "ε"),
+      ];
 
-    const alphabet = new Alphabet(["ε"]);
-    const nfa = new NFA(
-      [stateA, stateB, stateC],
-      transitions,
-      stateA,
-      [stateC],
-      alphabet,
-    );
+      const alphabet = new Alphabet(["ε"]);
+      const nfa = new NFA(
+        [stateA, stateB, stateC],
+        transitions,
+        stateA,
+        [stateC],
+        alphabet,
+      );
 
-    const dfa = nfa.toDFA();
-
-    expect(dfa.states).toHaveLength(1);
-    expect(dfa.states[0].name).toBe("A,B,C");
-    expect(dfa.startState!.name).toBe("A,B,C");
-    expect(dfa.acceptStates).toEqual(dfa.states);
-    expect(dfa.transitions).toHaveLength(0);
+      const closure = nfa["epsilonClosure"](stateA);
+      expect(closure.size).toBe(3);
+      expect(closure.has(stateA)).toBeTruthy();
+      expect(closure.has(stateB)).toBeTruthy();
+      expect(closure.has(stateC)).toBeTruthy();
+    });
   });
 
-  test("should convert an NFA with multiple states and transitions to a DFA", () => {
-    const stateA = new State("A");
-    const stateB = new State("B");
-    const stateC = new State("C");
+  describe("move method", () => {
+    test("should compute move correctly", () => {
+      const stateA = new State("A");
+      const stateB = new State("B");
+      const stateC = new State("C");
 
-    const transitions = [
-      new Transition(stateA, stateB, "0"),
-      new Transition(stateB, stateC, "1"),
-      new Transition(stateC, stateA, "0"),
-      new Transition(stateA, stateC, "1"),
-      new Transition(stateB, stateA, "0"),
-    ];
+      const transitions = [
+        new Transition(stateA, stateB, "0"),
+        new Transition(stateA, stateC, "1"),
+        new Transition(stateB, stateC, "1"),
+      ];
 
-    const alphabet = new Alphabet(["0", "1"]);
-    const nfa = new NFA(
-      [stateA, stateB, stateC],
-      transitions,
-      stateA,
-      [stateC],
-      alphabet,
-    );
+      const alphabet = new Alphabet(["0", "1"]);
+      const nfa = new NFA(
+        [stateA, stateB, stateC],
+        transitions,
+        stateA,
+        [stateC],
+        alphabet,
+      );
 
-    const dfa = nfa.toDFA();
-    expect(dfa.states).toHaveLength(4); // Number of DFA states will vary depending on the NFA
-    expect(dfa.startState!.name).toBe("A");
-    expect(dfa.acceptStates.map((state) => state.name)).toContain("C");
-    expect(dfa.transitions).toHaveLength(8); // Adjust depending on the expected number of DFA transitions
-  });
-
-  test("should handle removing unreachable states in DFA", () => {
-    const stateA = new State("A");
-    const stateB = new State("B");
-    const stateC = new State("C");
-
-    const transitions = [
-      new Transition(stateA, stateB, "0"),
-      new Transition(stateB, stateC, "1"),
-      new Transition(stateC, stateA, "0"),
-      new Transition(stateA, stateC, "1"),
-      new Transition(stateB, stateA, "0"),
-      new Transition(stateC, stateB, "1"),
-    ];
-
-    const alphabet = new Alphabet(["0", "1"]);
-    const nfa = new NFA(
-      [stateA, stateB, stateC],
-      transitions,
-      stateA,
-      [stateC],
-      alphabet,
-    );
-
-    const dfa = nfa.toDFA(true);
-
-    expect(dfa.states).toHaveLength(3); // Should only include reachable states
-    expect(dfa.startState!.name).toBe("A");
-    expect(dfa.acceptStates.map((state) => state.name)).toContain("C");
-    expect(dfa.transitions).toHaveLength(6); // Adjust depending on the expected number of DFA transitions
+      const moveResult = nfa["move"](new Set([stateA, stateB]), "1");
+      expect(moveResult.size).toBe(1);
+      expect(moveResult.has(stateC)).toBeTruthy();
+    });
   });
 });
