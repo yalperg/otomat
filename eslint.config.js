329c54cacaa95beb1fb973908d5d98021d8098d5
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2024-08-30 23:25:23 +0300

    chore: remove eslintignore file

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
+  { ignores: ["coverage", "public", "dist", "pnpm-lock.yaml"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
