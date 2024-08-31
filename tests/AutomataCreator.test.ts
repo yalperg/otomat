a4099dc5f3abd1f5ab2637ce394efb9f5e4fbc01
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2024-08-31 11:59:39 +0300

    test: add automataCreator test

---
+++ tests/AutomataCreator.test.ts
@@ -0,0 +1,57 @@
+import AutomataCreator from "../src/lib/AutomataCreator";
+import DFA from "../src/lib/DFA";
+import NFA from "../src/lib/NFA";
+import { FSAJSON } from "../src/types";
+
+describe("AutomataCreator", () => {
+  test("createFromJSON creates a DFA", () => {
+    const dfaJSON: FSAJSON = {
+      states: ["q0", "q1"],
+      alphabet: ["0", "1"],
+      transitions: [
+        { from: "q0", to: "q1", symbol: "0" },
+        { from: "q0", to: "q0", symbol: "1" },
+        { from: "q1", to: "q1", symbol: "0" },
+        { from: "q1", to: "q0", symbol: "1" },
+      ],
+      startState: "q0",
+      acceptStates: ["q1"],
+    };
+
+    const result = AutomataCreator.createFromJSON(dfaJSON);
+    expect(result).toBeInstanceOf(DFA);
+  });
+
+  test("createFromJSON creates an NFA (with epsilon)", () => {
+    const nfaJSON: FSAJSON = {
+      states: ["q0", "q1"],
+      alphabet: ["0", "1"],
+      transitions: [
+        { from: "q0", to: "q1", symbol: "0" },
+        { from: "q0", to: "q0", symbol: "0" },
+        { from: "q0", to: "q1", symbol: "ε" },
+      ],
+      startState: "q0",
+      acceptStates: ["q1"],
+    };
+
+    const result = AutomataCreator.createFromJSON(nfaJSON);
+    expect(result).toBeInstanceOf(NFA);
+  });
+
+  test("createFromJSON creates an NFA (without epsilon)", () => {
+    const nfaJSON: FSAJSON = {
+      states: ["q0", "q1"],
+      alphabet: ["0", "1"],
+      transitions: [
+        { from: "q0", to: "q1", symbol: "0" },
+        { from: "q0", to: "q0", symbol: "1" },
+      ],
+      startState: "q0",
+      acceptStates: ["q1"],
+    };
+
+    const result = AutomataCreator.createFromJSON(nfaJSON);
+    expect(result).toBeInstanceOf(NFA);
+  });
+});
