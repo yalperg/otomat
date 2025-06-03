3d736167064a79a25b158ad95c4230fccf9fa399
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2025-06-03 22:00:53 +0300

    feat: add base error classes for automata-related errors

---
+++ src/errors/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Base error class for all automata-related errors.
+ */
+export class AutomataError extends Error {
+  /**
+   * @param message Error message
+   * @param cause Optional underlying error
+   */
+  constructor(
+    message: string,
+    public cause?: Error,
+  ) {
+    super(message);
+    this.name = 'AutomataError';
+    if (cause) this.stack += '\nCaused by: ' + cause.stack;
+  }
+}
+
+/**
+ * Error thrown for invalid automaton definitions or operations.
+ */
+export class InvalidAutomatonError extends AutomataError {
+  constructor(message: string, cause?: Error) {
+    super(message, cause);
+    this.name = 'InvalidAutomatonError';
+  }
+}
